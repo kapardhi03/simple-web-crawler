e28e7a3437ca6f3d15996437a13b44723348d6a2
kapardhi03 <118623227+kapardhi03@users.noreply.github.com>
2023-10-25 16:28:50 +0530

    Updated

--- src/parser.js
+++ src/parser.js
@@ -2,35 +2,35 @@ import HTMLParser from 'node-html-parser';
 import { createLink } from './utils.js';
 
 export const extractInformation = (domain, html) => {
-  const root = HTMLParser.parse(html);
+    const root = HTMLParser.parse(html);
 
-  const rawLinks = root
-    .querySelectorAll('a')
-    .map((node) => node.attributes['href']);
+    const rawLinks = root
+        .querySelectorAll('a[href]')
+        .map((node) => node.attributes['href']);
 
-  // Removes duplicate links
-  const setOfLinks = new Set(rawLinks);
+    // Removes duplicate links
+    const setOfLinks = new Set(rawLinks);
 
-  const urls = Array.from(setOfLinks).map((link) => {
-    // to check for undefined or null in links array
-    if (link) {
-      let url;
-      if (link.startsWith('http://') || link.startsWith('https://')) {
-        try {
-          url = createLink(link);
-        } catch (e) {
-          url = null;
+    const urls = Array.from(setOfLinks).map((link) => {
+        // to check for undefined or null in links array
+        if (link) {
+            let url;
+            if (link.startsWith('http://') || link.startsWith('https://')) {
+                try {
+                    url = createLink(link);
+                } catch (e) {
+                    url = null;
+                }
+            } else if (link.startsWith('/')) {
+                url = createLink(`${domain}${link}`);
+            }
+            return url;
         }
-      } else if (link.startsWith('/')) {
-        url = createLink(`${domain}${link}`);
-      }
-      return url;
-    }
-  });
+    });
 
-  return {
-    title: root.querySelector('body h1').rawText,
-    body: root.querySelector('body').rawText,
-    links: urls,
-  };
+    return {
+        title: root.querySelector('body h1').rawText,
+        body: root.querySelector('body').rawText,
+        links: urls,
+    };
 };
